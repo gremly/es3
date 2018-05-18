3cbaace574a5975c9440926722cbc9cc80a57845
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2018-05-17 23:57:25 -0500

    Convert chunks reading in a parallel call, update keys generation

--- apps/es3/src/es3_cluster.erl
+++ apps/es3/src/es3_cluster.erl
@@ -11,10 +11,21 @@
 
 -spec read(Name :: iodata()) -> binary() | {error, Reason :: any()}.
 read(Name) ->
-    [{_, Name, NodesChunks}] = es3_repo:read(Name),
-    Chunks = lists:map(fun({Node, Keys}) -> rpc:call(Node, es3_chunk, list, [Keys]) end, NodesChunks),
-    BinList = lists:map(fun({_, BinChunk}) -> BinChunk end, lists:flatten(Chunks)),
-    binary:list_to_bin(BinList).
+    case es3_repo:read(Name) of
+        [] -> {error, file_not_found};
+        [{_, Name, NodesChunks}] ->
+            %Chunks = lists:map(fun({Node, Keys}) -> rpc:call(Node, es3_chunk, list, [Keys]) end, NodesChunks),
+            ChunksMap = lists:foldl(fun({Node, Keys}, Acc) ->
+                                         Nodes = [Node | maps:get(nodes, Acc)],
+                                         KeysList  = maps:get(keys, Acc) ++ Keys,
+                                         Map   = maps:put(nodes, Nodes, Acc),
+                                         maps:put(keys, KeysList, Map)
+                                 end, #{nodes => [], keys => []}, NodesChunks),
+            {Res, _} = rpc:multicall(maps:get(nodes, ChunksMap), es3_chunk, list, [maps:get(keys, ChunksMap)]),
+            SortedList = lists:sort(fun({KeyA, _}, {KeyB, _}) -> KeyA > KeyB end, lists:flatten(Res)),
+            BinList = lists:map(fun({_, BinChunk}) -> BinChunk end, SortedList),
+            binary:list_to_bin(BinList)
+    end.
 
 delete(Name) -> ok.
 
@@ -35,17 +46,17 @@ distribute_chunks(Nodes, Chunks) ->
 
 write_chunks([], _, _, NodesChunks) -> NodesChunks;
 write_chunks([Node|Rest], GroupedChunks, Idx, NodesChunks) ->
-    NodeInfo = node_chunks(Node, element(Idx, GroupedChunks), []),
+    NodeInfo = node_chunks(Node, element(Idx, GroupedChunks), Idx, []),
     write_chunks(Rest, GroupedChunks, Idx + 1, [NodeInfo | NodesChunks]).
 
-node_chunks(Node, [], Keys)    -> {Node, Keys};
-node_chunks(Node, [H|T], Keys) ->
-    Key = gen_key(Node),
+node_chunks(Node, [], _, Keys)    -> {Node, Keys};
+node_chunks(Node, [H|T], Idx, Keys) ->
+    Key = gen_key(Idx),
     rpc:cast(Node, es3_chunk, write, [Key, H]),
-    node_chunks(Node, T, [Key|Keys]).
+    node_chunks(Node, T, Idx, [Key|Keys]).
 
-gen_key(Node) -> 
-    atom_to_list(Node) ++ integer_to_list(timestamp()).
+gen_key(Prefix) ->
+    integer_to_list(Prefix) ++ integer_to_list(timestamp()).
 
 %% Function adapted from https://gist.github.com/DimitryDushkin/5532071
 timestamp() ->
